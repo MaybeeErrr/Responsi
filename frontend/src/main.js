fe289de08e24d4c6d132a1ec2aa67149e309f9b8
MaybeeErrr <erioktafianto11@gmail.com>
2025-07-04 14:55:50 +0700

    sd

---
+++ frontend/src/main.js
@@ -0,0 +1,9 @@
+// src/main.js
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import animateOnScroll from './directives/animateOnScroll'; // <-- Imporconst app = createApp(App);
+app.directive('animate-on-scroll', animateOnScroll); // <-- Daftarkan
+app.use(router);
+app.mount('#app');
