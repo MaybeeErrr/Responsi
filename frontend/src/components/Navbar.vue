82f457f3bbb0bded043bdd81e0cdacfdca9662d7
MaybeeErrr <erioktafianto11@gmail.com>
2025-07-11 16:14:12 +0700

    Responsi

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="bg-white shadow-lg sticky top-0 z-50 animate__animated animate__fadeInDown">
     <nav class="container mx-auto px-6 py-4 flex justify-between items-center">
-      <div class="text-2xl font-extrabold text-gray-900 tracking-wide">A R H | Portofolio</div>
+      <div class="text-2xl font-extrabold text-gray-900 tracking-wide">MaybeeErrr|Portofolio</div>
       <ul class="flex space-x-8">
         <li><router-link to="/" class="text-gray-700 hover:text-blue-600 transform hover:-translate-y-1 transition-all duration-300 font-medium text-lg">Profil</router-link></li>
         <li><router-link to="/education" class="text-gray-700 hover:text-blue-600 transform hover:-translate-y-1 transition-all duration-300 font-medium text-lg">Pendidikan</router-link></li>
